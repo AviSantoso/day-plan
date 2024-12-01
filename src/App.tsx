70d5abc4c080b7229002cb42ed9608597abaef02
Avi Santoso <avi.santoso@gmail.com>
2024-12-01 10:13:03 +0800

    feat: simplify

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,13 @@ interface ITimeEntry {
 }
 
 const startHour = 8;
-const endHour = 22;
+const endHour = 21;
 
 const hours: string[] = [];
 
 for (let i = startHour; i <= endHour; i++) {
   hours.push(`${i}:00`);
-  hours.push(`${i}:30`);
+  // hours.push(`${i}:30`);
 }
 
 function to12HourFormat(time: string) {
@@ -34,31 +34,29 @@ function App() {
 
   return (
     <div className="w-screen h-screen">
-      <div className="max-w-screen-lg w-full p-2 mx-auto">
-        <div className="flex flex-col w-full">
-          {timeEntries.map((entry, index) => {
-            return (
-              <div
-                key={entry.time}
-                className={clsx("flex items-center px-4 py-2 cursor-pointer")}
-              >
-                <div className="text-xs w-20">{to12HourFormat(entry.time)}</div>
-                <div className="text-sm flex-1">
-                  <input
-                    type="text"
-                    className="w-full p-2 px-3 bg-gray-100 text-gray-800"
-                    value={entry.description}
-                    onChange={(e) => {
-                      setTimeEntries((draft) => {
-                        draft[index].description = e.target.value;
-                      });
-                    }}
-                  />
-                </div>
+      <div className="flex flex-col w-full h-full p-4">
+        {timeEntries.map((entry, index) => {
+          return (
+            <div
+              key={entry.time}
+              className={clsx("flex flex-1 items-center px-4 py-2")}
+            >
+              <div className="text-xs w-20">{to12HourFormat(entry.time)}</div>
+              <div className="text-sm flex-1">
+                <input
+                  type="text"
+                  className="w-full p-2 px-3 bg-gray-100 text-gray-800"
+                  value={entry.description}
+                  onChange={(e) => {
+                    setTimeEntries((draft) => {
+                      draft[index].description = e.target.value;
+                    });
+                  }}
+                />
               </div>
-            );
-          })}
-        </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
